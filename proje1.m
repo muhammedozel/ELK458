89e4fe9432df9babfe1c8b78d1534d183a14b01b
Muhammed Özel <ozelmuh@itu.edu.tr>
2023-05-03 02:20:16 +0300

    fixed

--- proje1.m
+++ proje1.m
@@ -143,8 +143,19 @@ inductances = {
            'A750MW187M1J(1E020', 60, 0.020, 4.70;
            'UCZ1J181M1MS',       60, 0.200, 9.00;
            'EKY-630ELL681MK40S', 60, 0.021, 1.81;
-           'EKY-630ELL471ML20S', 60, 0.025, 1.73
+           'EKY-630ELL471ML20S', 60, 0.025, 1.73;
            };
+
+% En etkili INDUCTANCE'i seç
+loss_weight = 0.7;
+cost_weight = 0.3;
+[best_inductance, best_score] = select_best_inductance(inductances, loss_weight, cost_weight);
+
+% En etkili INDUCTANCE'i yazdır
+fprintf('En Etkili INDUCTANCE: %s\n', best_inductance{1});
+fprintf('Gerilim (V): %.2f V\n', best_inductance{2});
+fprintf('ESR: %.2f\n', best_inductance{3});
+fprintf('Maliyet: %.2f\n', best_inductance{4});
 
 % En etkili INDUCTANCE seçim algoritması
 function [best_inductance, best_score] = select_best_inductance(inductances, loss_weight, cost_weight)
@@ -163,14 +174,3 @@ function [best_inductance, best_score] = select_best_inductance(inductances, los
         end
     end
 end
-
-% En etkili INDUCTANCE'i seç
-loss_weight = 0.7;
-cost_weight = 0.3;
-[best_inductance, best_score] = select_best_inductance(inductances, loss_weight, cost_weight);
-
-% En etkili INDUCTANCE'i yazdır
-fprintf('En Etkili INDUCTANCE: %s\n', best_inductance{1});
-fprintf('Gerilim (V): %.2f V\n', best_inductance{2});
-fprintf('ESR: %.2f\n', best_inductance{3});
-fprintf('Maliyet: %.2f\n', best_inductance{4});
